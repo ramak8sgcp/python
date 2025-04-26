6e92e828d484229cfc3ec2c13a6bcc97bfb8b9c9
ramak8sgcp <ramak8sgcp@gmail.com>
2025-04-26 20:01:53 +0530

    data types

---
+++ hello.py
@@ -0,0 +1,72 @@
+# print ("Hello World")
+
+#Variables
+#1.Integer
+#2.Float / double
+#3.Strings
+#4.Boolean
+#5.List
+#6.Tuples
+#7.Dictonary
+#8.Sets
+
+# res = (10 + 20)
+# print("Result:", res)
+# print(10+30)
+# print("Memory Address:" type(res))
+
+
+# res= 10 + 20
+# print("result: ", res)
+# print("memory Address: ", id(res))
+# # print("Result:", res, "type:", type(res))
+# print ("data type:", type(res)) 
+
+# a = 10.23
+# b = 20.23
+# res = a + b
+# print("Result: ", res, "Type: ", type(res) )
+
+# sample_str = "This is A string"
+# print(sample_str, type(sample_str))
+# print(dir(sample_str))
+
+"""capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 
+'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 
+'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 
+'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 
+'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 
+'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 
+'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 
+'swapcase', 'title', 'translate', 'upper', 'zfill']
+"""
+# print(sample_str.capitalize())
+# print(sample_str.casefold())
+# print(sample_str.center())
+# txt = "Hello My Name Is PETER"
+
+# x = txt.swapcase()
+# x = txt.split(' ')
+
+
+# print(x)
+# print(txt.lower())
+# print(txt.find('M'))
+
+#Python uses zero indexing
+# sample_str = "Hello"
+# print(sample_str.index('H'))
+# print(sample_str.index('o'))
+
+# sample_str = "Hello"
+# sample_char = sample_str[-1]
+# print(sample_char)
+# print(len(sample_str))
+
+# Boolen
+# ...........
+a = True
+b = False
+
+print(a and b) #ans: False
+print(a or b)   # ans: True
